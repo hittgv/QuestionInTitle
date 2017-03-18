ef49c3cff50991d6a16c969094337cbb22ab2cc8
Bhasin, Krishan <Krishan.Bhasin@capitalone.com>
2017-03-18 18:02:42 +0000

    i think this works

--- questionInTitle/routes/returnFeature.js
+++ questionInTitle/routes/returnFeature.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* POST home page. */
+router.post('/', function(req, res, next) {
+
+  console.log()
+
+  var html = req.body.html;
+
+  var heading = html.split("<h1")[1].split(">")[1].split("</h1")[0];
+
+  var title = html.split("<title")[1].split(">")[1].split("</title")[0]
+
+  console.log(heading);
+  console.log(title);
+
+
+  var returnObject = {containsQuestionmark:false}
+
+  if (title.contains("?") || heading.contains("?")){
+    returnObject.containsQuestionmark=true;
+  }
+
+  res.send(JSON.stringify(returnObject));
+
 });
 
 module.exports = router;
